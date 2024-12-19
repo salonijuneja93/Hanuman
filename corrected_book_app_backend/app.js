35fdb8edc7553152621953dbd454cfed6bf73277
salonijuneja93 <salonijuneja93@gmail.com>
2024-12-19 02:21:23 -0800

    Update app.js

--- corrected_book_app_backend/app.js
+++ corrected_book_app_backend/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const mongoURI = process.env.MONGO_URI || "mongodb+srv://salonijuneja93:salonijuneja93@saloni07.6xpr2.mongodb.net/";
+const mongoURI = process.env.MONGO_URI || "mongodb+srv://Abc1234:salonijuneja93@saloni07.6xpr2.mongodb.net/";
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.error(err));
